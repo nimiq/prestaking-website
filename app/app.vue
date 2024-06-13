5441cc792b1a8e699d79d1f1fb0d7a796af17232
Maxi <maximogarciamtnez@gmail.com>
2024-06-13 12:46:49 +0300

    chore: updated to nuxt v3.12
    Also enabled flag to be compatible with future nuxt 4

---
+++ app/app.vue
@@ -0,0 +1,52 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+h1 {
+  /* Promotional Desktop / H1 */
+  font-family: Mulish;
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 76.8px; /* 120% */
+}
+
+h2 {
+  /* Promotional Desktop / H2 */
+  font-family: Mulish;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 57.6px; /* 120% */
+}
+
+h3 {
+  /* 1440/H3 */
+  font-family: Mulish;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 130%; /* 36.4px */
+}
+
+.subline {
+  /* 1440/Subline */
+  font-family: Mulish;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 36px */
+}
+
+summary {
+  color: var(--Dark-Blue, #1f2348);
+  font-family: Mulish;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 36px; /* 150% */
+}
+</style>
