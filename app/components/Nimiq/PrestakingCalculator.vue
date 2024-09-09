1fe31691d8d88091bdcc00e0582df7dd75954c0c
Sören <hello@soerenschwert.de>
2024-09-09 14:08:15 +0200

    Set staking calculator embed

--- app/components/Nimiq/PrestakingCalculator.vue
+++ app/components/Nimiq/PrestakingCalculator.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-  <div class="z-3 col-span-3 row-span-3 rounded-8 bg-green md:col-span-2 md:row-span-3" />
+  <iframe
+    src="https://www.nimiq.com/staking-calculator/?layout=prestaking-embed"
+    class="w-full h-full z-3 col-span-3 row-span-3 rounded-8 md:col-span-2 md:row-span-3"
+  />
 </template>
 
 <style>
