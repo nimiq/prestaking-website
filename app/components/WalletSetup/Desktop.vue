ca4cd49c2249bbfd1cf822e0d7e6727f9a8f3aae
onmax <maximogarciamtnez@gmail.com>
2024-09-30 18:20:09 +0200

    images dynamic

--- app/components/WalletSetup/Desktop.vue
+++ app/components/WalletSetup/Desktop.vue
@@ -7,24 +7,30 @@ const activeIndex = ref(0)
 <template>
   <div flex="~" rounded-l-16 border="1 neutral/20" bg="neutral/2" p="$p" pr-0 style="--p: 48px">
     <div relative flex-1>
-      <div absolute inset-y="[calc(var(--p)*-1)]" right-0 w-315 bg-gradient="to-l from-[#e9e9eb] to-transparent" />
+      <div inset-y="[calc(var(--p)*-1)]" pointer-events-none absolute right-0 w-315 bg-gradient="to-l from-[#e9e9eb] to-transparent" />
 
       <ul flex="~ col gap-24">
-        <li v-for="({ title, description }, i) in items" :key="i">
-          <div flex="~ gap-16">
-            <div text="18 neutral center" size-30 shrink-0 rounded-full bg-neutral-400 flex="~ items-center justify-center" font-semibold>
-              {{ i + 1 }}
+        <li v-for="({ title, description }, i) in items" :key="i" w-full flex>
+          <button bg-transparent @click="activeIndex = i">
+            <div flex="~ gap-16">
+              <div text="18 neutral center" size-30 shrink-0 rounded-full bg-neutral-400 flex="~ items-center justify-center" font-semibold>
+                {{ i + 1 }}
+              </div>
+              <h3 text-left>
+                {{ title }}
+              </h3>
             </div>
-            <h3>
-              {{ title }}
-            </h3>
-          </div>
-          <p mt-12 text="18 neutral-800">
-            {{ description }}
-          </p>
+            <p mt-12 text="left 18 neutral-800">
+              {{ description }}
+            </p>
+          </button>
         </li>
       </ul>
     </div>
-    <NuxtImg :src="items[activeIndex].img" border="1 neutral/20" my="[calc((var(--p)+32px)*-1)]" w-437 rounded-16 shadow />
+    <transition mode="out-in" enter-active-class="transition-[opacity,filter] duration-400" enter-from-class="op-0 blur-2" enter-to-class="op-100 blur-0" leave-active-class="transition-[opacity,filter] duration-150 blur-0" leave-from-class="op-100" leave-to-class="op-0 blur-2">
+      <div :key="activeIndex" h-558 w-437 of-hidden rounded-16 border="1 neutral/20" my="[calc((var(--p)+32px)*-1)]" shadow>
+        <NuxtImg :src="items[activeIndex].img" />
+      </div>
+    </transition>
   </div>
 </template>
